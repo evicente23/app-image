4fe6bbaad459ac62273f8cce529f5153b2f6f59a
Angel Santana <50668561+Rafaelinpsercom@users.noreply.github.com>
2020-03-06 16:53:23 -0500

    cambios2

--- kia/portalkia/chatbot.js
+++ kia/portalkia/chatbot.js
@@ -1,7 +1,3 @@
-document.write(
-  '<script type="text/javascript" src="https://service.force.com/embeddedservice/5.0/esw.min.js" ></script>'
-);
-
 var initESW = function(gslbBaseURL) {
   embedded_svc.settings.displayHelpButton = true; //Or false
   embedded_svc.settings.language = 'es'; //For example, enter 'en' or 'en-US'
